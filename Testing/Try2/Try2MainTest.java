85a31af4a09816ebcdf49b638bebf7b33338eea8
Richard Davidsson <r-1chard@hotmail.com>
2022-10-25 14:23:19 +0200

    Program v2 Added Tests fixed test

--- Testing/Try2/Try2MainTest.java
+++ Testing/Try2/Try2MainTest.java
@@ -2,44 +2,60 @@ package Try2;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 class Try2MainTest {
-
+    String testFile = "Testing/Try2/Customertest";
     Try2Main testProgram = new Try2Main();
-
     @Test
     void getName() {
-        String line = "7703021234, Alhambra Aromes";
-        assert (testProgram.getName(line).trim()).equals("Alhambra Aromes");
-        assert !(testProgram.getName(line)).equals("7703021234");
-        assert !(testProgram.getName(line)).equals("7703021234, Alhambra Aromes");
+        try (Scanner filescanner = new Scanner(new File(testFile))) {
+            String line = filescanner.nextLine();
+            assert(testProgram.getName(line).trim().equals("Alhambra Aromes"));
+            assert!(testProgram.getName(line).trim().equals("7703021234"));
+            assert!(testProgram.getName(line).trim().equals("7703021234, Alhambra Aromes"));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     void getIdNumber() {
-        String line = "7703021234, Alhambra Aromes";
-        assert (testProgram.getIdNumber(line).trim().equals("7703021234"));
-        assert !(testProgram.getIdNumber(line).equals("Alhambra Aromes"));
-        assert !(testProgram.getIdNumber(line).equals("7703021234, Alhambra Aromes"));
+        try (Scanner filescanner = new Scanner(new File(testFile))){
+            String firstLine = filescanner.nextLine();
+            String secondLine = filescanner.nextLine();
+            assert (testProgram.getIdNumber(firstLine).trim().equals("7703021234"));
+            assert !(testProgram.getIdNumber(firstLine).trim().equals("Alhambra Aromes"));
+            assert !(testProgram.getIdNumber(firstLine).trim().equals("7703021234, Alhambra Aromes"));
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
 
     }
 
     @Test
     void getLastPaymentDate() {
-        String line = "2022-07-01";
-        assert (testProgram.getLastPaymentDate(line).equals(LocalDate.of(2022,7,1)));
-        assert !(testProgram.getLastPaymentDate(line).equals(LocalDate.of(2022,7,2)));
-        assert !(testProgram.getLastPaymentDate(line).equals(LocalDate.of(2022,2,2)));
-
+        try (Scanner filescanner = new Scanner(new File(testFile))){
+            String firstLine = filescanner.nextLine();
+            String secondLine = filescanner.nextLine();
+            assert (testProgram.getLastPaymentDate(secondLine).equals(LocalDate.of(2022,7,1)));
+            assert !(testProgram.getLastPaymentDate(secondLine).equals(LocalDate.of(2022,7,2)));
+            assert !(testProgram.getLastPaymentDate(secondLine).equals(LocalDate.of(2021,7,1)));
+        } catch (FileNotFoundException e) {
+           e.printStackTrace();
+        }
     }
 
     @Test
     void isMember() {
-        LocalDate date = LocalDate.of(2022,7,1);
+        LocalDate date = LocalDate.now();
         assertTrue (testProgram.isMember(date));
         assertFalse(testProgram.isMember(date.minusYears(2)));
+        assertFalse(testProgram.isMember(date.minusYears(1).minusDays(1)));
 
     }
 }
